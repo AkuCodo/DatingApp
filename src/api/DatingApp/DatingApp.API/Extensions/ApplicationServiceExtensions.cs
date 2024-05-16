9a0b90be56ce34d131efc0b46b163ce0f0e1ecc8
vinicius <88739721+vinic-32@users.noreply.github.com>
2024-05-16 12:06:44 -0300

    Initial commit

---
+++ src/api/DatingApp/DatingApp.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Interfaces;
+using DatingApp.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
