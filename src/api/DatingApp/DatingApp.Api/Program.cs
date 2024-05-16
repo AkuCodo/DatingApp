c74e8dc77586a25f62587877939acd6d228c03e6
vinicius <88739721+vinic-32@users.noreply.github.com>
2024-05-16 16:31:45 -0300

    FIX: Accidental delete of the API layer.

---
+++ src/api/DatingApp/DatingApp.Api/Program.cs
@@ -0,0 +1,25 @@
+using System.Text;
+using DatingApp.Ioc.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("*"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
