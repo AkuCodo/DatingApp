0b174ee04debf208696213a545fe5b68199bb86e
vinicius <88739721+vinic-32@users.noreply.github.com>
2024-05-21 15:44:27 -0300

    Changes on the client application

--- src/web/client/src/app/nav/nav.component.ts
+++ src/web/client/src/app/nav/nav.component.ts
@@ -1,33 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../services/account.service';
+import { User } from '../models/user';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
-  model: any = {};
-  loggedIn = false;
+export class NavComponent implements OnInit {
+  model: any = { };
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void { 
-    this.getCurrentUser();
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    })
-  }
+  } 
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
@@ -35,6 +27,5 @@ export class NavComponent {
 
   logout() { 
     this.accountService.logout();
-    this.loggedIn = false;
   }
 }
